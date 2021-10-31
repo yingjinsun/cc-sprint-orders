5f4bac38c32383086b361ad323998134529787f5
cheng09 <czhang43@buffaloledu>
2021-10-31 15:16:35 -0400

    [mod] put order path back

--- OrderModule/orders/urls.py
+++ OrderModule/orders/urls.py
@@ -7,7 +7,7 @@ from orders import views
 urlpatterns = {
     # ex: /infos/orders
 
-    # path('orders', views.orders, name='orders'),
+    path('orders', views.orders, name='orders'),
 
     re_path(r'^orders/$', views.ordersWithField, name='ordersWithField'),
     # ex: /infos/orders/1
