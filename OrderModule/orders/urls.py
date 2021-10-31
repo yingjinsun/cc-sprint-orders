012017f01c258e69ef82922847828063a96336d5
cheng09 <czhang43@buffaloledu>
2021-10-31 19:20:04 -0400

    [fix] fix url

--- OrderModule/orders/urls.py
+++ OrderModule/orders/urls.py
@@ -7,9 +7,9 @@ from orders import views
 urlpatterns = {
     # ex: /infos/orders
 
-    path('orders', views.orders, name='orders'),
+    # path('orders', views.orders, name='orders'),
 
-    re_path(r'^orders/$', views.ordersWithField, name='ordersWithField'),
+    re_path(r'^orders', views.ordersWithField, name='ordersWithField'),
     # ex: /infos/orders/1
     path('orders/<int:order_id>', views.certainOrder, name='getOrderByOrderId'),
 
